170409dee3448c149bf4ec226e1d05c59c4a34e3
ganeshshinde94 <ganesh.shinde@students.tamuk.edu>
2020-07-08 15:51:05 -0700

    updated code

--- Kerberized_HDFS.py
+++ Kerberized_HDFS.py
@@ -3,6 +3,7 @@ import requests
 import logging
 
 logger = logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 session = requests.Session()
 session.verify = False
@@ -11,5 +12,4 @@ client = KerberosClient(url='http://x.x.x.x:abcd', session=session,
 mutual_auth='REQUIRED',principal='abcdef@LMNOPQ')
 
 lists = client.list('/user/')
-
 logger.debug(lists)
